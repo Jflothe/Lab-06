9a0ad8696c9292a8130a6c117bac74841812a931
Jflothe <Jhflothe@gmail.com>
2023-03-09 12:41:10 -0500

    Wrote up main function

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-def encode():
-    #write encode function up here
-    #
+def encode(password):
+    pass
 
-
-
-
-
-
+def print_menu():
+    print("Menu")
+    print("-------------")
+    print("1. Encode", "\n2. Decode", "\n3. Quit")
 
 def main():
-    pass
+    while True:
+        print_menu()
+        print()
+        option = input("Please enter an option: ")
+        if option == 1:
+            password = input("Please enter your password to encode: ")
+            encoded_p = encode(password)
+            print("Your password has been encoded and stored!")
+        if option == 2:
+            decoded_p = decode(password)
+            print(f"The encoded password is {encoded_p}, and the original password is {decoded_p}.")
+        if option == 3:
+            break
+        else:
+            print("That is not a valid option. Try again")
+
 
 
 
